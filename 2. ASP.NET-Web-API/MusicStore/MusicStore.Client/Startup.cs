aa1ba5dc8eb1e6d83131ccbaba176ceb5e7874e7
dchakov <dchakov@yahoo.com>
2015-10-29 20:05:00 +0200

    initial

---
+++ 2. ASP.NET-Web-API/MusicStore/MusicStore.Client/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace MusicStore.Client
+{
+    using Models;
+    using MusicStore.Data;
+    using MusicStore.Data.Migrations;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MusicStoreDbContext, Configuration>());
+
+            //var db = new MusicStoreDbContext();
+            //db.Albums.Count();
+
+            var client = new HttpClient()
+            {
+                BaseAddress = new Uri("http://localhost:57271/")
+            };
+
+            GetAllAlbums(client);
+
+        }
+
+        private static void GetAllAlbums(HttpClient client)
+        {
+            using (client)
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response =  client.GetAsync("api/Album").Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var albums = response.Content.ReadAsAsync<IEnumerable<Album>>().Result;
+                    foreach (var album in albums)
+                    {
+                        Console.WriteLine("{0,4} {1,-20} {2}",
+                            album.Id, album.Title, album.Producer);
+                    }
+                }
+                else
+                    Console.WriteLine("{0} ({1})",
+                        (int)response.StatusCode, response.ReasonPhrase);
+
+            }
+        }
+    }
+}
---
+++ 2. ASP.NET-Web-API/MusicStore/MusicStore.Services/Controllers/AlbumController.cs
@@ -0,0 +1,101 @@
+﻿namespace MusicStore.Services.Controllers
+{
+    using Data.Repositories;
+    using Models;
+    using MusicStore.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    public class AlbumController : ApiController
+    {
+        private IGenericRepository<Album> repository = null;
+
+        public AlbumController()
+        {
+            this.repository = new GenericRepository<Album>();
+        }
+
+        public AlbumController(IGenericRepository<Album> repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return this.Ok(this.repository.SelectAll());
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            return this.Ok(this.repository.SelectById(id));
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody]AlbumRequestModel album)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var newAlbum = new Album
+            {
+                Title = album.Title,
+                Year = album.Year,
+                Producer = album.Producer,
+                Artists = album.Artists,
+                Songs = album.Songs
+            };
+
+            this.repository.Insert(newAlbum);
+            this.repository.SaveChanges();
+
+            return this.Ok(newAlbum);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(int id, [FromBody]AlbumRequestModel album)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.BadRequest(this.ModelState);
+            }
+
+            var currentAlbum = this.repository.SelectById(id);
+
+            if (currentAlbum == null)
+            {
+                return this.BadRequest("Invalid Id");
+            }
+
+            currentAlbum.Title = string.IsNullOrEmpty(album.Title) ? currentAlbum.Title : album.Title;
+            currentAlbum.Year = album.Year;
+            currentAlbum.Producer = string.IsNullOrEmpty(album.Producer) ? currentAlbum.Producer : album.Producer;
+
+            this.repository.Update(currentAlbum);
+            this.repository.SaveChanges();
+            return this.Ok(currentAlbum);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var album = this.repository.SelectById(id);
+
+            if (album == null)
+            {
+                return this.BadRequest("Invalid id");
+            }
+
+            this.repository.Delete(id);
+            this.repository.SaveChanges();
+            return this.Ok();
+        }
+    }
+}
